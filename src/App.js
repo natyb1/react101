b44d9063a2385e84fbe0e24046027ed1920325a2
Natnael <68988452+natyb1@users.noreply.github.com>
2023-11-04 10:25:22 +0300

    sarted new udemy full course

--- src/App.js
+++ src/App.js
@@ -1,58 +1,12 @@
 import React from "react";
-import Employee from "./Components/PrEmp/Employee";
-import { useState } from "react";
 
 const App = () => {
-  const [employee, setEmployee] = useState([
-    {
-      id: 1,
-      name: "John Doe",
-      role: "Software Engineer",
-      image: "https://source.unsplash.com/random",
-    },
-    {
-      id: 2,
-      name: "Jane Doe",
-      role: "Product Manager",
-      image: "https://source.unsplash.com/random",
-    },
-    {
-      id: 3,
-      name: "Peter Smith",
-      role: "Designer",
-      image: "https://source.unsplash.com/random",
-    },
-    {
-      id: 4,
-      name: "Mary Johnson",
-      role: "Marketing Manager",
-      image: "https://source.unsplash.com/random",
-    },
-    {
-      id: 5,
-      name: "Michael Jones",
-      role: "Sales Manager",
-      image: "https://source.unsplash.com/random",
-    },
-  ]);
-
-  function updateEmployee() {
-    console.log("hello from app.js");
-  }
-
-  const empElement = employee.map((employee) => {
-    return (
-      <Employee
-        key={employee.id}
-        id={employee.id}
-        name={employee.name}
-        role={employee.role}
-        updateEmployee={updateEmployee}
-      />
-    );
-  });
-
-  return <div className="flex justiify-between flex-wrap"> {empElement}</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <button>Get Advice</button>
+    </div>
+  );
 };
 
 export default App;
